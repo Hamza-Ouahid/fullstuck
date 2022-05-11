d4b2d00690584bdf60d1b562d067790df05597c1
Last <98898187+Hamza-Ouahid@users.noreply.github.com>
2022-05-11 12:06:36 +0000

    React Router + Get all
    react router dom + adding a new page for getting all employees

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import {  ChakraProvider, Heading} from "@chakra-ui/react";
 import * as React from "react";
 import Form from "./Components/Form";
+import Header from "./Components/Header";
+import Employees from "./Components/Employees";
+import {BrowserRouter as Router,Switch,Route,Routes} from "react-router-dom";
 function App() {
   return (
     <ChakraProvider>
-    <Heading as="h1" size="xl" textAlign="center">
-      Welcom to the app
-    </Heading>
-    <Form />
+      <Router>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Employees/>}> </Route>
+    <Route path="/Add" element={ <Form />}></Route>
+   
+    </Routes>
+    </Router>
   </ChakraProvider>
     );
 }
---
+++ frontend/src/Components/Employees.jsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Button,
+} from '@chakra-ui/react'
+import { useState,useEffect } from "react";
+
+export default function Employees() {
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    const getEmployees = async () => {
+      const employeesFromServer = await fetchEmployees();
+     
+      setEmployees(employeesFromServer);
+      
+    };
+    getEmployees();
+  }, []);
+  
+  const fetchEmployees = async () => {
+    const res = await fetch("http://localhost:8080/employee/getAll");
+    const data = await res.json();
+    return data;
+    
+  };
+
+  console.log(employees);
+
+
+
+  return (
+    <TableContainer>
+    <Table variant='striped' colorScheme='gray'>
+      <TableCaption>List of Employees</TableCaption>
+      <Thead>
+        <Tr>
+          <Th>Id</Th>
+          <Th>First Name</Th>
+          <Th>Last Name</Th>
+          <Th>Integration Date</Th>
+          <Th>Registration Number</Th>
+          <Th>Bunsiness Unite</Th>
+          <Th>Phone Number</Th>
+          <Th>Position</Th>
+          <Th>Responsible</Th>
+        </Tr>
+      </Thead>
+       <Tbody>
+      {employees.map((employee)=>{
+        return(
+         <Tr key ={employee.id}>
+        <Td>{employee.id}</Td>
+         <Td>{employee.first_name}</Td>
+         <Td>{employee.last_name}</Td>
+         <Td>{employee.intigration_date}</Td>
+         <Td>{employee.registration_number}</Td>
+         <Td>{employee.business_unit}</Td>
+         <Td>{employee.phone_number}</Td>
+         <Td>{employee.position}</Td>
+         <Td>{employee.responsible}</Td>
+         <Td ><Button>Update</Button></Td>
+       </Tr>
+        )
+
+      })}
+      
+       
+      </Tbody>
+    </Table>
+  </TableContainer>
+  )
+}
